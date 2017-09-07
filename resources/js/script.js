5319ddc4eca7fa5198316e3c10968fff708353a6
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-09-07 10:35:23 +0100

    Set up EventListener JavaScript DOM

---
+++ resources/js/script.js
@@ -0,0 +1,14 @@
+var suits = ["spades", "hearts", "clubs", "diams"];
+var cardFace = ["2","3","4","5","6","7","8","9"];
+var deck = [];
+var players = [[],[]];
+
+var fightButton = document.querySelector("#btnBattle");
+
+// event listeners
+fightButton.addEventListener('click',battle);
+
+//functions
+function battle(){
+    console.log('works');
+}
